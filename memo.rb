e1c5511ad2f2af6c412d3a4c07d62b5f6238cfb4
EC2 Default User <ec2-user@ip-172-31-28-204.ap-northeast-1.compute.internal>
2022-11-17 13:56:12 +0000

    改行コードを実装してみました。

--- memo.rb
+++ memo.rb
@@ -2,14 +2,14 @@ require "csv" # CSVファイルを扱うためのライブラリを読み込ん�
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 
-memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
+memo_type = gets.chomp.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
 if memo_type == 1
     puts "拡張子を除いたファイルを入力してください"
     
-    file_name = gets
+    file_name = gets.chomp
     
     puts "メモしたい内容を記入してください終了後、Ctrl + D　を押します。"
     
@@ -24,7 +24,7 @@ elsif memo_type == 2
   
   puts "既存のメモを編集します"
   puts "拡張子を除いた既存ファイルを入力してください"
-  file_name = gets
+  file_name = gets.chomp
   puts "編集したい内容を入力してください。完了したらCtrl+Dを押します"
   memo = STDIN.read
   
